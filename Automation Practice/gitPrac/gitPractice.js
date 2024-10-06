879a84028226755d64eefc23bf8ba3dfb5f0793f
Mahir Shadid <mahir.shadid@gmail.com>
2024-10-06 16:13:37 +0600

    Changed the product name to be global variable

--- Automation Practice/gitPrac/gitPractice.js
+++ Automation Practice/gitPrac/gitPractice.js
@@ -3,14 +3,14 @@ const { Builder, By, until, Key } = require('selenium-webdriver');
 (
     async function gitPractice() {
         let driver = await new Builder().forBrowser('chrome').build();
-
+        let pname = 'nike';
         try{
             await driver.get('https://demo.evershop.io/');
 
             await driver.findElement(By.xpath('//a[@class="search-icon"]')).click();
 
             let product = await driver.wait(until.elementLocated(By.xpath('//input[@type="text" and @placeholder="Search"]')),10000);
-            await product.sendKeys('revolution', Key.ENTER);
+            await product.sendKeys(`${pname}`, Key.ENTER);
 
             let avail = await driver.wait(until.elementLocated(By.xpath('//span[@class="product-count italic block mt-8"]')),10000).getText();
             if(avail[0] == 0){
